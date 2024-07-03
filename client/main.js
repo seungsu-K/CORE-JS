6f1c3faa5ebe88ab5d9d70e4b914abb8647f3817
seungsu-K <kss095@naver.com>
2024-07-03 10:01:42 +0900

    learn. connectedCallback

--- client/main.js
+++ client/main.js
@@ -1,44 +1,21 @@
-/* global gsap */
+class MyElement extends HTMLElement {
+  constructor() {
+    super();
+  }
 
-import {
-  clearContents,
-  deleteStorage,
-  getNode,
-  getStorage,
-  setStorage,
-} from './lib/index.js';
+  connectedCallback() {
+    console.log('탄생함');
+  }
 
-// xhrPromise.get('https://jsonplaceholder.typicode.com/users').then(console.log);
-
-// const getData = async () => {
-//   const data = await xhrPromise.get(
-//     'https://jsonplaceholder.typicode.com/users'
-//   );
-//   console.log(data);
-// };
-
-// getData();
-
-const ENDPOINT = 'http://localhost:3000/users';
-
-getStorage('text').then((res) => {
-  textField.value = res;
-  // 새로고침 했을 때 기본값으로 남겨줌
-});
-
-const textField = getNode('#textField');
-const clear = getNode('button[data-name="clear"]');
-
-function handleTextField() {
-  const value = this.value;
-
-  setStorage('text', value);
+  disconnectedCallback() {
+    console.log('죽음');
+  }
 }
 
-function handleClear() {
-  deleteStorage('text');
-  clearContents(textField);
-}
+customElements.define('c-element', MyElement);
 
-textField.addEventListener('input', handleTextField);
-clear.addEventListener('click', handleClear);
+const elem = document.createElement('c-element');
+
+const app = document.getElementById('app');
+
+app.appendChild(elem);
