b13566833aaef2b2e8961397737bd2e5a6b8913f
seungsu-K <kss095@naver.com>
2024-07-03 10:39:35 +0900

    learn. customized built-in Element

--- client/main.js
+++ client/main.js
@@ -1,38 +1,11 @@
-class Button extends HTMLElement {
+class Button extends HTMLButtonElement {
   constructor() {
     super();
-
-    this.button = document.querySelector('button');
   }
 
-  connectedCallback() {
-    this._render();
-  }
+  connectedCallback() {}
 
   disconnectedCallback() {}
-
-  static get observedAttributes() {
-    return ['id'];
-  }
-
-  attributeChangedCallback(name, oldValue, newValue) {
-    console.log(name, oldValue, newValue);
-    if (oldValue !== newValue) {
-      this._render();
-    }
-  }
-
-  _render() {
-    this.button.textContent = this.id;
-  }
 }
 
-customElements.define('c-button', Button);
-
-const c = document.querySelector('c-button');
-
-let count = 0;
-
-c.addEventListener('click', () => {
-  c.setAttribute('id', ++count);
-});
+customElements.define('c-button', Button, { extends: 'button' });
